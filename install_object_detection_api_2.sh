d987eb5defa921c94b0e653cb145c89c8e5cd50d
dellius-alexander <dellius.alexander@gmail.com>
2021-11-03 19:06:37 -0400

    added some  error handling for build environment

--- install_object_detection_api_2.sh
+++ install_object_detection_api_2.sh
@@ -2,6 +2,7 @@
 #####################################################################
 # This script install the OBJECT DETECTION API on top of Tensorflow
 ## Prerequisite: you may need to install additional dependencies such as:
+# apt-get update -y && apt-get install -y \
 # git \
 # gpg-agent \
 # python3-cairocffi \
@@ -13,51 +14,90 @@
 # gcc \
 # build-essentials
 #####################################################################
-### USAGE: bash install_object_detection_api_2.sh [Enter the Models Directory Repo Path you Downloaded Above [models_dir]]
+### USAGE: bash install_object_detection_api_2.sh \
+######  
+######
+######  Parameters: ${1} = [PATH to Tensorflow Models Directory [models]]
+######              ${2} = [Your OS flavor]
 #####################################################################
 # Instructions on how to build object detection model API
 # https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2.md
 # mkdir -p models_dir
 # git clone https://github.com/tensorflow/models.git models_dir/
-# Perspective of execution:=> From the root of the git repository [models_dir/]
+# Perspective of execution:=> From the root of the git repository ["models/"]
 #### Type the Name of your Models Repo Directory
-set -e
-MODELS_DIR_NAME=${1:-"Models"}
+set -e  # Start execution from an isolated clean environment using "set -e".
+MODELS_DIR_NAME=${1:-"Models"} # file discriptor $1
+FLAVOR=${2:-"bionic"}  # file discriptor $2
 MODELS_DIR=$(find ~- -type d -name "${MODELS_DIR_NAME}")
 RESEARCH_DIR=${MODELS_DIR}/research
-printf """\n
+JSON_PARAM=("""
 {\n
-\t'Models dir name': '${MODELS_DIR_NAME}',\n \
-\t'Models directory': '${MODELS_DIR}',\n \
-\t'Research directory': '${RESEARCH_DIR}'\n \
+    'Models dir name': '${MODELS_DIR_NAME}',\n\
+    'Models directory': '${MODELS_DIR}',\n\
+    'Research directory': '${RESEARCH_DIR}',\n\
+    'OS Flavor': '${FLAVOR}'\n\
 }\n
-"""
-
+""")
+#####################################################################
+#####################################################################
 # Download Tensorflow Models Repo if it don't exist
 if [ ! -d "${MODELS_DIR}" ]  && [ ! -h "${MODELS_DIR}" ]; then
-    mkdir -p ${MODELS_DIR_NAME}/
-    git clone https://github.com/tensorflow/models.git  ${MODELS_DIR_NAME}/
+    # create the models directory
+    mkdir -p ${PWD}/${MODELS_DIR_NAME}/
+    # clone the Models Repo
+    git clone https://github.com/tensorflow/models.git  ${PWD}/${MODELS_DIR_NAME}/
+    # Define some environment variables
     MODELS_DIR=$(find ~- -type d -name "${MODELS_DIR_NAME}")
     RESEARCH_DIR=${MODELS_DIR}/research
-    printf """\n
-    {\n
-    \t'Models dir name': '${MODELS_DIR_NAME}',\n \
-    \t'Models directory': '${MODELS_DIR}',\n \
-    \t'Research directory': '${RESEARCH_DIR}'\n \
-    }\n
-    """
 fi;
+printf "${JSON_PARAM}"
+#####################################################################
+# Replace with your Linux OS Flavor, e.g. [OS=Flavor] ~ [ubuntu20.04=focal]
 # sed -i 's/old-text/new-text/g' input.txt
-sed -i 's/$(lsb_release -c -s)/bionic/g' Models/research/object_detection/dockerfiles/tf2/Dockerfile
-# # exit 0
-# cat >>${MODELS_DIR}/research/object_detection/dockerfiles/tf2/Dockerfile<<EOF
-# \n
-# ARG MODELS_DIR_NAME="Models"
-# ARG MODELS_DIR="/home/tensorflow/models"
-# ARG RESEARCH_DIR="/home/tensorflow/models/research"
-# EOF
-# Build the docker container to run the Object Detection API
-docker build -f ${MODELS_DIR}/research/object_detection/dockerfiles/tf2/Dockerfile -t object-detection:0.1 ${MODELS_DIR}
+# sed -i '1i\
+# first_line_text
+# '
+DOCKERFILE="${MODELS_DIR}/research/object_detection/dockerfiles/tf2/Dockerfile"
+echo $DOCKERFILE
+# exit if Dockerfile not found
+if [ ! -f "${DOCKERFILE}" ]; then
+    printf "The Dockerfile does not exist please check your repo at: \n\t${DOCKERFILE}\n"
+    exit 2
+# check if build args are present in Dockerfile
+elif [ -f "${DOCKERFILE}" ] && [[ $(cat $DOCKERFILE | grep -ic "ARG FLAVOR='${FLAVOR}'") -gt 0 ]]; then
+    echo "Docker Build Args Set......"
+    printf "\tARG FLAVOR='${FLAVOR}'\n"
+# add build args to after "FROM" statement in Dockerfile if not present
+elif [ -f "${DOCKERFILE}" ] && [[ $(cat $DOCKERFILE | grep -ic "ARG FLAVOR='${FLAVOR}'") -eq 0 ]]; then
+    sed "/FROM tensorflow\/tensorflow/ a \
+    ARG FLAVOR='${FLAVOR}'
+    " $DOCKERFILE | tee $DOCKERFILE
+fi;
+# now check for post-modified Dockerfile and update it if has not been modified
+if [ -f "${DOCKERFILE}" ] && [[ $(cat $DOCKERFILE | grep -ic 'cloud-sdk-${FLAVOR}') -eq 0 ]]; then
+    sed -i 's/$(lsb_release -c -s)/${FLAVOR}/g' $DOCKERFILE
+    printf "\nModified Dockerfile:\n\t$(cat $DOCKERFILE | grep -i 'cloud-sdk-${FLAVOR}' | awk '{print $3}')\n"
+else
+    printf "Dockerfile has already been modified for the correct CLOUD-SDK-${FLAVOR}: \
+        \n\t$(cat $DOCKERFILE | grep -i 'cloud-sdk-${FLAVOR}' | awk '{print $3}')\n"
+fi;
+# kill any duplicate running containers
+CONTAINER=$(docker ps | grep -i 'object-detection' | awk '{print $1}')
+if [ -z "${CONTAINER}" ]; then # check for running container
+    # check for stopped containers
+    CONTAINER=$(docker ps -a | grep -i 'object-detection' | awk '{print $1}')
+    if [ ! -z "${CONTAINER}" ]; then
+       docker rm "${CONTAINER}" # remove the stopped container
+    fi;
+    # no container then so nothing to do but move on
+
+else # kill and remove the running container
+    docker kill "${CONTAINER}" && docker rm "${CONTAINER}"
+fi;
+# Use the 'docker build' to build the Object Detection API
+docker build --build-arg=FLAVOR=${FLAVOR} -t object-detection:0.1 \
+    -f ${MODELS_DIR}/research/object_detection/dockerfiles/tf2/Dockerfile ${MODELS_DIR}
 # Now RUN the Object Detection Image "od" you created above
 docker run -it -v ${MODELS_DIR}:/home/tensorflow/models/research:rw -d --name object-detection object-detection:0.1
 # change directory and compile Object Detection API protocols
@@ -73,6 +113,7 @@ cp ${MODELS_DIR}/research/object_detection/packages/tf2/setup.py ${RESEARCH_DIR}
 # install the built module into your python/conda environment 
 python3 -m pip install --use-feature=2020-resolver ${RESEARCH_DIR}
 # Kill docker  object-detection container
-docker stop object-detection && docker rm object-detection
+docker stop object-detection && \
+docker rm object-detection
 # Test the installation.
 python3 ${RESEARCH_DIR}/object_detection/builders/model_builder_tf2_test.py
